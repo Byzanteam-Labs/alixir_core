d34ab2de1bd344b0626859d399117484e0f6c77e
Phil Chen <06fahchen@gmail.com>
2018-08-14 21:55:20 +0800

    Add get method perform

--- lib/alixir/request.ex
+++ lib/alixir/request.ex
@@ -16,8 +16,20 @@ defmodule Alixir.Request do
   @spec perform(%Request{}) :: {:ok, status_code, body} | {:error, reason}
   def perform(
     %Request{http_method: http_method, url: url, params: params, headers: headers, body: body}
-  ) do
+  ) when http_method in ~w{put delete}a do
     case HTTPoison.request(http_method, url, body, headers, params: List.wrap(params)) do
+      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
+        {:ok, 200, body}
+      {:ok, %HTTPoison.Response{body: body, status_code: status_code}} ->
+        {:ok, status_code, body}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
+  end
+
+  @spec perform(%Request{http_method: :get, url: String.t()}) :: {:ok, status_code, body} | {:error, reason}
+  def perform(%Request{http_method: :get, url: url})  do
+    case HTTPoison.request(:get, url) do
       {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
         {:ok, 200, body}
       {:ok, %HTTPoison.Response{body: body, status_code: status_code}} ->
