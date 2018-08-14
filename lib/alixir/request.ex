74820a8bf88eecdaa39d0da4b4ccdac15de7e454
Phil Chen <06fahchen@gmail.com>
2018-08-14 22:02:01 +0800

    Abstract out do_perfom

--- lib/alixir/request.ex
+++ lib/alixir/request.ex
@@ -17,19 +17,16 @@ defmodule Alixir.Request do
   def perform(
     %Request{http_method: http_method, url: url, params: params, headers: headers, body: body}
   ) when http_method in ~w{put delete}a do
-    case HTTPoison.request(http_method, url, body, headers, params: List.wrap(params)) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        {:ok, 200, body}
-      {:ok, %HTTPoison.Response{body: body, status_code: status_code}} ->
-        {:ok, status_code, body}
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
-    end
+    do_perfom(http_method, url, params: params, headers: headers, body: body)
   end
 
   @spec perform(%Request{http_method: :get, url: String.t()}) :: {:ok, status_code, body} | {:error, reason}
   def perform(%Request{http_method: :get, url: url})  do
-    case HTTPoison.request(:get, url) do
+    do_perfom(:get, url)
+  end
+
+  defp do_perfom(method, url, options \\ []) do
+    case HTTPoison.request(method, url, options) do
       {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
         {:ok, 200, body}
       {:ok, %HTTPoison.Response{body: body, status_code: status_code}} ->
